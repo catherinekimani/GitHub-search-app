6d8bcae541f9f8e13049bb4d2cff0a31ed03dd87
catherinekimani <catherine.kimani@student.moringaschool.com>
2022-04-17 16:36:15 +0300

    Add slight changes

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,15 +1,21 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
+  private userName: string;
 
-  constructor(private http: HttpClient) { }
-  
-  // user datails
-  getRepos() {
-    return this.http.get("https://api.github.com/users/catherinekimani/repos")
+  constructor(private http: HttpClient) { 
+    console.log("service")
+    this.userName = "catherinekimani"
+  }
+  // userinfo
+  getUserInfo() {
+    return this.http.get("https://api.github.com/users/" + this.userName).pipe(map(data => {
+      return data;
+    }))
   }
 }
