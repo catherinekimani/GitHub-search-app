a85bbc97d5d8dbfbd546d62b63791575f6c9e5d4
catherinekimani <catherine.kimani@student.moringaschool.com>
2022-04-17 12:07:24 +0300

    Add github api

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+  
+  // user datails
+  getRepos() {
+    return this.http.get("https://api.github.com/users/catherinekimani/repos")
+  }
 }
