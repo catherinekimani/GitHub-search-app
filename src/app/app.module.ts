bdd3a40451efa5332b1aa07f74d726d180aed031
catherinekimani <catherine.kimani@student.moringaschool.com>
2022-04-17 09:32:24 +0300

    Import HttpClientModule - to perfom http requests

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { UsersComponent } from './users/users.component';
 import { ReposComponent } from './repos/repos.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { ReposComponent } from './repos/repos.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
