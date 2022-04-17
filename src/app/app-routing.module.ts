a02740a75a3261b42deae8b7ebd7a7a9d0ade6aa
catherinekimani <catherine.kimani@student.moringaschool.com>
2022-04-18 02:40:46 +0300

    Add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ReposComponent } from './repos/repos.component';
+import { UsersComponent } from './users/users.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "users", component: UsersComponent },
+  { path: "repos", component: ReposComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
