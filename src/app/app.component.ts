0d17dc75112b3b02c54f2e7a1cf1b4c23870d2f7
catherinekimani <catherine.kimani@student.moringaschool.com>
2022-04-17 12:06:10 +0300

    Import Api service - to get user repos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from './services/api.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'GitHub-serch-app';
+
+  constructor(private apiService:ApiService) {
+    
+  }
+  ngOnInit(): void {
+    this.apiService.getRepos().subscribe((Repos) => 
+      console.log(Repos)
+    );
+  }
 }
