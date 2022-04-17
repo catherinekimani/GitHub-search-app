46b850ca86b67474d2587aa3b16c006f9972150e
catherinekimani <catherine.kimani@student.moringaschool.com>
2022-04-17 09:08:37 +0300

    Import users and repo component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { UsersComponent } from './users/users.component';
+import { ReposComponent } from './repos/repos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    UsersComponent,
+    ReposComponent
   ],
   imports: [
     BrowserModule,
