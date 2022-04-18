c95476bda12c2cb611f6bef2a868859ff8eef73c
catherinekimani <catherine.kimani@student.moringaschool.com>
2022-04-19 01:31:57 +0300

    Auto-generated commit [ci skip]

--- src/app/directives/my-app.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { MyAppDirective } from './my-app.directive';
-
-describe('MyAppDirective', () => {
-  it('should create an instance', () => {
-    const directive = new MyAppDirective();
-    expect(directive).toBeTruthy();
-  });
-});
--- src/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  apiKey: "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"
-};
